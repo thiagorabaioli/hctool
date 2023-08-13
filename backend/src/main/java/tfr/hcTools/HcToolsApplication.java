565a6a6fc570be1f100e24a89d7b66325f4b01cf
thiagorabaioli <tfrabaioli@outlook.com>
2023-08-13 16:04:01 +0100

    First commit

---
+++ backend/src/main/java/tfr/hcTools/HcToolsApplication.java
@@ -0,0 +1,13 @@
+package tfr.hcTools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HcToolsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/tfr/hcTools/HcToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package tfr.hcTools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
